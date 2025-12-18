ae4e1b9f07b8bbd486a53b2c1c082d25e5b6eb98
Neha Abid <neyhaabid@gmail.com>
2025-12-18 21:39:16 +0500

    Add files via upload

---
+++ Task 3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Item \t Price \t Quality \n";
+	cout<<"Pen \t $1 \t 10 \n";
+	cout<<"Pencil \t $2 \t 5 \n";
+	cout<<"Eraser \t $0.5 \t 20 \n";
+	return 0;
+}
---
+++ Task 4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"She said, \"Lets learn C++!\" "<<endl;
+	cout<<"To start save your file in C:\C++_Projects\\Lab1"<<endl;
+	cout<<"Goog Luck!\n";
+	return 0;
+}
